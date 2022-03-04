02ab02ca81ef2cda5ee7987dad7de18c619ad525
kaalrok <92065902+kaalrok@users.noreply.github.com>
2022-03-04 09:55:01 +0100

    updating files and folders

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,30 +1,25 @@
-## Deliverable 1
-
-# Step 3 Load the library
-library(dplyr)
-
-# Read in the CSV file.
-MechaCar <- read.csv(file="MechaCar_mpg.csv")
+### Deliverable 1 ###################
 
 # Step 3 Load the library
 library(dplyr)
 
 # Step 4. Read in the CSV file.
-MechaCar <- read.csv(file="MechaCar_mpg.csv")
+# Working Path set to the Data Folder
+MechaCar_mpg <- read.csv(file='MechaCar_mpg.csv',check.names = F,stringsAsFactors = F)
 
 # Step 5. Create linear model
-MechaCar_lm <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar)
+Mecha_lm <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar)
 
 # Step 6. Use the Summary function to determine the p-value
-summary(MechaCar_lm)
+summary(Mecha_lm)
 
 # r Squared Value
-MechaCar_rSquared <- summary(MechaCar_lm)$r.squared
+MechaCar_rSquared <- summary(Mecha_lm)$r.squared
 MechaCar_rSquared
 
 
 ---
-# Deliverable 2
+### Deliverable 2 ###################
 
 # Step 2: 
 MCSuspension <- read.csv(file="Suspension_Coil.csv")
@@ -38,4 +33,27 @@ total_summary
 
 lot_summary <- MCSuspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
 lot_summary
+
+
+### Deliverable 3 ###################
+# Note: Level of significance is .05
+
+# Ho: mu = 1500
+# Ha: mu <> 1500
+
+t.test(Suspension_Coil$PSI, mu=1500)
+
+# Analysis - p-value (0.06) greater than 0.05 therefore we fail to reject Ho
+
+#t.test by lots
+# Lot 1
+t.test(subset(Suspension_Coil, Manufacturing_Lot=="Lot1")$PSI, mu=1500)
+
+# Lot 2
+t.test(subset(Suspension_Coil, Manufacturing_Lot=="Lot2")$PSI, mu=1500)
+
+# Lot 3
+t.test(subset(Suspension_Coil, Manufacturing_Lot=="Lot3")$PSI, mu=1500)
+
+
 
